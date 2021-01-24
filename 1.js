7dca81187c70fb169c0cdfed367d6475e9de43cf
wangjun <726938457@qq.com>
2021-01-24 20:32:17 +0800

    1

---
+++ 1.js
@@ -0,0 +1,31 @@
+// Bilili私信
+
+// function start() {
+//     i = setInterval(() => {
+//         for(let i = 0; i < 30; i++) {
+//             document.getElementsByClassName('send-box')[0].__vue__.content = '输入值';
+//             document.getElementsByClassName('btn-box send-btn active')[0].click();   
+//             console.log(i);
+//         }
+//     }, 500)
+// }
+
+// function stop() {
+//     clearInterval(i)
+// }
+
+
+// 微博回复
+
+// function start() {
+//     i = setInterval(() => {
+//         var a = document.getElementsByClassName('W_input');
+//         a[2].value = a[2].value + '输入内容'
+//         document.getElementsByClassName('W_btn_a')[0].click();   
+//         console.log(i);
+//     }, 2000)
+// }
+
+// function stop() {
+//     clearInterval(i)
+// }
